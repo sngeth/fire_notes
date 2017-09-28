47ef018f275ec938488f9e0c1ecd41fe26b725ad
Sid Ngeth <sngeth@gmail.com>
2017-09-28 09:34:03 -0400

    Initial commit

---
+++ lib/fire_notes.ex
@@ -0,0 +1,18 @@
+defmodule FireNotes do
+  @moduledoc """
+  Documentation for FireNotes.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FireNotes.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fire_notes_test.exs
@@ -0,0 +1,8 @@
+defmodule FireNotesTest do
+  use ExUnit.Case
+  doctest FireNotes
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
